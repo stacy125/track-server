a613c363c6a30ddc67e001b0dd6347867ea127e3
stacy leitstein <unique523@yahoo.com>
2022-02-21 15:11:39 -0400

    setup mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 app.get('/', (req, res) => {
     res.send('Hi there.')
 });
+
+const mongoURI = 'mongodb+srv://stacy:udemytracker@cluster0.9wdeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoURI)
+mongoose.connection.on('connected', () => {
+    console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error("Error connecting to mongo", error);
+});
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
