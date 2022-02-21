af25dd3e5b2a712b6a21b944aab91bac65481f85
stacy leitstein <unique523@yahoo.com>
2022-02-21 17:44:41 -0400

    created userSchema with jwt and bcrypt and authRoutes, created middleware to make sure there is a token, created trackSchema and trackRoutes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose')
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hi there.')
-});
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
 
 const mongoURI = 'mongodb+srv://stacy:udemytracker@cluster0.9wdeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(mongoURI)
@@ -15,6 +24,9 @@ mongoose.connection.on('error', (error) => {
     console.error("Error connecting to mongo", error);
 });
 
+app.get("/", requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
